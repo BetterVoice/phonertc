921555a916a5cc66a144cfa4d2ae837dfbc1534f
Thomas Quintana <thomas@bettervoice.com>
2014-12-03 13:13:30 -0800

    Revert "Debugging"
    This reverts commit 5de0e14915b36f45c333d5f603922f826e549e28.

--- www/phonertc.js
+++ www/phonertc.js
@@ -58,8 +58,6 @@ function Session(config) {
   }
 
   function onSendMessage(data) {
-    window.console.log('Data Received: ');
-    window.console.log(data);
     if (data.type === '__answered') {
       callEvent('answer');
     } else if(data.type === '__disconnected') {
